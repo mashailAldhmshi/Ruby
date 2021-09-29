ef90d17ebb0deb4eec3c3143d5504fd483779548
mashailAldhmshi <m.alenzi@hungerstation.com>
2021-09-29 18:02:58 +0300

    Ruby on Rails

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+  Rails.application.routes.draw do
+    root "articles#index"
+
+    resources :articles
+  end
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+
